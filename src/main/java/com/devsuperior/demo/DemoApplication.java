518db4b4877533bce3c9a95677cd3a469ef038dd
Ital023 <contadoitalo911@gmail.com>
2024-08-05 20:10:27 -0300

    feat: Bcrypt

--- src/main/java/com/devsuperior/demo/DemoApplication.java
+++ src/main/java/com/devsuperior/demo/DemoApplication.java
@@ -1,13 +1,30 @@
 package com.devsuperior.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication implements CommandLineRunner {
+
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println(passwordEncoder.encode("123456"));
+
+		boolean result = passwordEncoder.matches("13456", "$2a$10$UQw.ATCfeSZtv3mYAMEm5ORSTww4GD/bhXSeAWPveuMekk5jzLC8C");
+		System.out.println(result);
+	}
+
 
 }

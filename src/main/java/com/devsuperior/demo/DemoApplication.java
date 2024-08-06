7c44893188e36669ab8413410ebdcfc7bc71e862
Ital023 <contadoitalo911@gmail.com>
2024-08-06 00:16:30 -0300

    feat: Access control by role, by route

--- src/main/java/com/devsuperior/demo/DemoApplication.java
+++ src/main/java/com/devsuperior/demo/DemoApplication.java
@@ -7,23 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
-public class DemoApplication implements CommandLineRunner {
-
-	@Autowired
-	private PasswordEncoder passwordEncoder;
-
+public class DemoApplication {
 
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-		System.out.println(passwordEncoder.encode("123456"));
-
-		boolean result = passwordEncoder.matches("13456", "$2a$10$UQw.ATCfeSZtv3mYAMEm5ORSTww4GD/bhXSeAWPveuMekk5jzLC8C");
-		System.out.println(result);
 	}
 
 
